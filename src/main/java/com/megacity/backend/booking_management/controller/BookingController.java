b264b79bd846490d75aa8aa2e4202055cade1fdd
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-01-24 11:07:12 +0530

    booking service basic architecture implemented

---
+++ src/main/java/com/megacity/backend/booking_management/controller/BookingController.java
@@ -0,0 +1,4 @@
+package com.megacity.backend.booking_management.controller;
+
+public class BookingController {
+}
---
+++ src/main/java/com/megacity/backend/domain/entity/Booking.java
@@ -0,0 +1,12 @@
+package com.megacity.backend.domain.entity;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+}
