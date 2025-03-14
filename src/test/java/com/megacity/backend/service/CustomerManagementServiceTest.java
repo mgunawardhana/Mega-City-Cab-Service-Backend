eaeeb42ca6ca0b68431f900411cc26af76d2e2ff
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-03-14 20:13:56 +0530

    sonar issue fixed

--- src/test/java/com/megacity/backend/service/CustomerManagementServiceTest.java
+++ src/test/java/com/megacity/backend/service/CustomerManagementServiceTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class CustomerManagementServiceTest {
+class CustomerManagementServiceTest {
 
     private JdbcTemplate writeJdbcTemplate;
     private JdbcTemplate readJdbcTemplate;
